058847749ffe407d58824863a01b6f073261f76c
Andrew <ajclinto@gmail.com>
2015-11-22 16:49:23 -0500

    Adding diffuse and firefox config. Add a script to link dot files.

---
+++ dotfiles/.mozilla/prefs.js
@@ -0,0 +1,5 @@
+user_pref("browser.startup.homepage", "https://weather.gc.ca/city/pages/on-143_metric_e.html");
+user_pref("browser.tabs.tabMinWidth", 1);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("mousewheel.min_line_scroll_amount", 100);
+user_pref("ui.key.menuAccessKeyFocuses", false);
